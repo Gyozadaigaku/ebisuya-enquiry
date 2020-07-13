bc76df68c81b5f8111bcff255bcd1c17d604dd1a
Jun Inoue <gaspardcube15@gmail.com>
2020-07-14 07:32:00 +0900

    add typing balls effect

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,12 +2,21 @@ import Avatar from "@material-ui/core/Avatar";
 import Ebisuke from "../assets/img/ebisuke.jpg";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TypingBalls } from "./index";
 
 const Chat = (props) => {
   const isQuestion = props.type === "question";
   const classes = isQuestion ? "p-chat__row" : "p-chat__reverse";
+
+  const [stage, setStage] = useState("TYPING");
+  const { text } = props;
+  useEffect(() => {
+    setStage("TYPING");
+    setTimeout(() => {
+      setStage("DONE");
+    }, 1000);
+  }, [text]);
 
   return (
     <>
@@ -17,21 +26,17 @@ const Chat = (props) => {
             <Avatar alt="icon" src={Ebisuke} />
           </ListItemAvatar>
         )}
-        {/* {setTimeout(function () {
-          return (
-            <div
-              className="p-chat__bubble"
-              dangerouslySetInnerHTML={{ __html: props.text }}
-            />
-          );
-        }, 1000)} */}
-        {/* <div className="p-chat__typing-balls-bubble">
-          <TypingBalls />
-        </div> */}
-        <div
-          className="p-chat__bubble"
-          dangerouslySetInnerHTML={{ __html: props.text }}
-        />
+
+        {isQuestion && stage === "TYPING" ? (
+          <div className="p-chat__typing-balls-bubble">
+            <TypingBalls />
+          </div>
+        ) : (
+          <div
+            className="p-chat__bubble"
+            dangerouslySetInnerHTML={{ __html: props.text }}
+          />
+        )}
       </ListItem>
     </>
   );
